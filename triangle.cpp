48538bacc74598bd6f0008298e561f41109bc576
Julia <usanevaulia@gmail.com>
2026-03-12 22:52:09 +0700

    Add a file triangle

--- triangle.cpp
+++ triangle.cpp
@@ -3,28 +3,27 @@
 using namespace std;
 
 int main(){
-	double a, b, c;
+    double a, b, c;
 
-	cout << "Введите стороны треугольника a b c: ";
-	cin >> a >> b >> c;
+    cout << "Введите стороны треугольника a b c: ";
+    cin >> a >> b >> c;
 
-	if(a + b <= c || a + c <= b || b + c <= a){
-		cout << "Ошибка. Треугольника с такими сторонами не существует" << endl;
-		return 1;
+    if (a + b <= c || a + c <= b || b + c <= a){
+        cout << "Ошибка. Треугольника c такими сторонами не существует" << endl;
+        return 1;
+    }
+
+    double perimeter = a + b + c;
+    cout << "Периметр: " << perimeter << endl;
+
+    double p = perimeter / 2;
+    double area = sqrt(p * (p - a) * (p - b) * (p - c));
+    cout << "Площадь: " << area << endl;
+
+    if (a == b || a == c || b == c){
+        cout << "Треугольник равнобедренный" << endl;
+    } else{
+        cout << "Треугольник не равнобедренный" << endl;
 	}
-
-	double perimeter = a + b + c;
-	cout << "Периметр: " << perimeter << endl;
-
-	double p = perimeter / 2;
-	double area = sqrt(p * (p - a) * (p - b) * (p - c));
-	cout << "Площадь: " << area << endl;
-
-	if(a == b || a == c || b == c){
-		cout << "Треугольник равнобедренный" << endl;
-	}else{
-		cout << "Треугольник не равнобедренный" << endl;
-	}
-
-	return 0;
+    return 0;
 }
